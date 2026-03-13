2654f534e0ef3913155cba29a84982bf8b5f56c4
edpedro <edp2013.ep@gmail.com>
2026-03-12 21:01:42 -0300

    crypto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as nodeCrypto from 'crypto';
 
 async function bootstrap() {
   try {
+    const g = globalThis as unknown as { crypto?: unknown };
+    if (!g.crypto) g.crypto = nodeCrypto as unknown;
+
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     await app.listen(process.env.PORT ?? 3000);
